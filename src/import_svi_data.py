9f0929daa546ef27e2d2ef886b06f580827488d6
notchia <lucia.korpas@gmail.com>
2021-10-20 17:47:54 -0400

    import and plot Chicago SVI shapefile

--- src/import_svi_data.py
+++ src/import_svi_data.py
@@ -1,5 +1,7 @@
 import os
 import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
 
 os.chdir('../')
 CWD = os.getcwd()
@@ -9,19 +11,27 @@ DATADIR = os.path.join(CWD, 'data/supplementary')
 def import_svi_data():
     """Import Chicago's SVI data for 2018"""
     
-    # Import raw CSV files (only FIPS codes from Chicago census tract file)
-    csv_svi = os.path.join(DATADIR, 'SVI_2018_Illinois.csv')
+    # Import FIPS codes from Chicago census tract file
     csv_censusTract = os.path.join(DATADIR, 'CensusTracts_2010_Chicago.csv')
-    data_svi_il = pd.read_csv(csv_svi)
-    censusTracts_chicago = pd.read_csv(csv_censusTract)['GEOID10']
+    chicagoFIPS = pd.read_csv(csv_censusTract)['GEOID10']
     
-    # Select only Chicago census tract SVI data
-    data_svi = data_svi_il[data_svi_il['FIPS'].isin(censusTracts_chicago)]
+    # Import SVI shapefile
+    shp = os.path.join(DATADIR, "SVI_2018_ILLINOIS_tract/SVI2018_ILLINOIS_tract.shp")
+    data_shp_il = gpd.read_file(shp)
+    data_shp_il['FIPS'] = data_shp_il['FIPS'].astype(int)
     
-    return data_svi
+    # Select Chicago data only
+    data_shp = data_shp_il.loc[data_shp_il['FIPS'].isin(chicagoFIPS)]
+    
+    # Plot geographic data for confirmation
+    data_shp.plot()
+    plt.savefig('figures/census_tracts_chicago.png', bbox_inches='tight', dpi=300)
+    
+    return data_shp
 
 
 # TODO: import list of Chicago AirNow sensors
 
+
 if __name__ == "__main__":
-    data_svi = import_svi_data()
+    data = import_svi_data()
